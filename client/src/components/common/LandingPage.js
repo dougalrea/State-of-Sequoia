8231059e25424837ab90a4f94675ae7fdc0e05cc
Dougal Rea <dougal.rea@gmail.com>
2021-07-02 21:36:24 +0100

    first pass at landing page done

--- client/src/components/common/LandingPage.js
+++ client/src/components/common/LandingPage.js
@@ -1,21 +1,63 @@
 import React from 'react'
-import { Center, Button } from '@chakra-ui/react'
-import { Link } from 'react-router-dom'
+import {
+  Center,
+  Button,
+  useDisclosure,
+  SlideFade,
+  Image,
+  Flex,
+  Box,
+  Spacer
+} from '@chakra-ui/react'
+import { useHistory } from 'react-router-dom'
 
 function LandingPage () {
+  const { isOpen, onToggle } = useDisclosure()
+  const history = useHistory()
+
   return (
     <>
-      <Center bg='green.100' h='100vh' color='white'>
-        <Link to='/home'>
-          <Button
-            variant='ghost'
-            color='orange.800'
-            _hover={{ boxShadow: 'md', bg: 'green.500', color: 'white' }}
-          >
-            Enter Sequoia
-          </Button>
-        </Link>
-      </Center>
+      <Box bg='green.100'>
+        <Flex>
+          <SlideFade in={!isOpen} offsetX='-20vw' unmountOnExit={true}>
+            <Box>
+              <Image
+                src='https://i.imgur.com/m5G07pE.png?3'
+                bg='green.100'
+                maxH='100vh'
+              />
+            </Box>
+          </SlideFade>
+          <Spacer />
+          <Center bg='green.100' color='white' h='100vh'>
+            {/* <Link to='/home'> */}
+            <Button
+              onClick={() => {
+                onToggle()
+                setTimeout(() => {
+                  history.push('/home')
+                }, 500)
+              }}
+              variant='ghost'
+              color='orange.800'
+              _hover={{ boxShadow: 'md', bg: 'green.600', color: 'white' }}
+            >
+              Enter Sequoia
+            </Button>
+            {/* </Link> */}
+          </Center>
+          <Spacer />
+          <SlideFade in={!isOpen} offsetX='20vw' unmountOnExit={true}>
+            <Box>
+              <Image
+                src='https://i.imgur.com/LXZJuQF.png?2'
+                bg='green.100'
+                maxH='100vh'
+              />
+            </Box>
+          </SlideFade>
+        </Flex>
+      </Box>
     </>
   )
 }
